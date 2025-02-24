c5a32e2a30cc3968fbb264da942d4359c1aab211
Morten <moab@eaaa.dk>
2025-02-24 21:09:20 +0100

    Version1

---
+++ src/pages/CreatePage.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import PostForm from "../components/PostForm";
+import { firebaseUrl } from "../firebase-config";
+
+export default function CreatePage() {
+    const navigate = useNavigate();
+
+    async function createPost(newPost) {
+        const url = `${firebaseUrl}/posts.json`;
+
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(newPost)
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("New post created: ", data);
+            navigate("/imageupload2/");
+        } else {
+            console.log("Sorry, something went wrong");
+        }
+    }
+
+    return (
+        <section className="page">
+            <h1>Create New Post</h1>
+            <PostForm savePost={createPost} />
+        </section>
+    );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import PostCard from "../components/PostCard";
+import { firebaseUrl } from "../firebase-config";
+
+export default function HomePage() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const url = `${firebaseUrl}/posts.json`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const postsArray = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
+            setPosts(postsArray);
+        }
+        getPosts();
+    }, []);
+
+    return (
+        <section className="page">
+            {posts.length > 0 ? (
+                <section className="grid-container">
+                    {posts.map(post => (
+                        <PostCard post={post} key={post.id} />
+                    ))}
+                </section>
+            )
+                :
+                (<p>Nothing to show</p>)
+            }
+        </section>
+    );
+}
